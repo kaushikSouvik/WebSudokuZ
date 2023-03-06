8033d1f2523addfc9cf69c1a2a29d6bc04112b5b
kaushikSouvik <websudokuproject@gmail.com>
2023-03-06 12:47:01 +0530

    Updated expres.static line

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs")
